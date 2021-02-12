4e2073b6588c6f1cd9139de98b637347a333726e
sijil <sijils@outlook.com>
2021-02-12 10:42:54 +0530

    final

--- src/components/movie-list/index.js
+++ src/components/movie-list/index.js
@@ -7,39 +7,44 @@ function MovieList() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [sent,setSent]=useState(false);
 
 
   // Note: the empty deps array [] means
   // this useEffect will run once
   // similar to componentDidMount()
   
+ 
   
-  const handleSubmit = (e) => {
-    fetch(`https://jsonmock.hackerrank.com/api/movies?Year=${e.target.value}`)
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setIsLoaded(true);
-        setItems(result.data);
-      },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      }
-    )
-  }
+  useEffect (()=>{
+    if(sent){
+      fetch(`https://jsonmock.hackerrank.com/api/movies?Year=${inputValue}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result.data);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      ).then(()=>setSent(false))
+    }
+   
+  })
 
   return (
     <div className="layout-column align-items-center mt-50">
       <section className="layout-row align-items-center justify-content-center">
-      <form onSubmit={handleSubmit} className="form-group">
         <input type="number" className="large" placeholder="Enter Year eg 2015" data-testid="app-input" 
-                         />
-        <button className="" type="submit" data-testid="submit-button" >Search</button>
-        </form>
+             value={inputValue} onChange={(e)=>setInputValue(e.target.value)}            />
+        <button className="" type="submit" onClick={()=>setSent(true)} data-testid="submit-button" >Search</button>
+      
       </section>
       {isLoaded?
       (items.length===0?<div>No Results Found</div>:(
